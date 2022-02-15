86805a15b8821d217437a94085aae648062352f4
r21dm <rockydwimakmur@gmail.com>
2022-02-16 02:04:22 +0700

    Convert HTML to React

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import Register from "./pages/register";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Register />
   </React.StrictMode>,
   document.getElementById("root")
 );
